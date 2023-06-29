2b2204b0cdef117fe67f697a0be9849ff780338a
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2023-06-29 08:17:15 -0600

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,11 @@
+print("Ingrese un numero:")
+caracteres = input()
+numero = int(caracteres)
+if(numero == 0):
+  print("Es Cero")
+else:
+  if(numero > 0): 
+    print("Es Positivo")
+  else:
+    print("Es Negativo")
+exit()
